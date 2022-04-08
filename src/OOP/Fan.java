c77301e2955dc631a218dd0d982abec34ca6d334
TangThiHoa <hoarua592001@gmail.com>
2022-04-08 08:17:43 +0700

    lan1

--- src/OOP/Fan.java
+++ src/OOP/Fan.java
@@ -1,10 +1,10 @@
 package OOP;
 
 public class Fan {
-    public final int SLOW = 1 , MEDIUM= 2  ,  FAST = 3 ;
-    private int speet = SLOW ;
+    public final int SLOW = 1, MEDIUM = 2, FAST = 3;
+    private int speet = SLOW;
     private boolean on = false;
-    public double radius = 5 ;
+    public double radius = 5;
     public String color = "blue ";
 
     public int getSLOW() {
@@ -35,7 +35,46 @@ public class Fan {
         return color;
     }
 
-//    public static void main(String[] args) {
-//
-//    }
+    public void setSpeet(int speet) {
+        this.speet = speet;
+    }
+
+    public void setOn(boolean on) {
+        this.on = on;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Fan() {
+    }
+
+    public String toString() {
+        if (on == true) {
+            return "Thong tin quat : " + this.speet + " "+ this.color + this.radius + "Quat bat ";
+        } else {
+            return "Thong tin quat : " + this.color + this.radius + "Quat bat ";
+        }
+    }
+
+    public static void main(String[] args) {
+        Fan fan1 = new Fan();
+        fan1.setSpeet(fan1.FAST);
+        fan1.setRadius(10);
+        fan1.setColor("yellow");
+        System.out.println(fan1);
+
+        Fan fan2 = new Fan();
+        fan2.setSpeet(fan2.MEDIUM);
+        fan2.setRadius(5);
+        fan2.setColor("blue");
+        System.out.println(fan2);
+
+
+    }
 }
